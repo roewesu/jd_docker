facd2afb278b70308dcbc2ab7ee1c0f47345e122
zhaozhanzhan <799316652@qq.com>
2021-07-12 13:35:47 +0800

    全民抢京豆

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -178,3 +178,5 @@
 12 0,6-23/2 * * * node /scripts/jd_summerMovement.js >> /scripts/logs/jd_summerMovement.log 2>&1
 #愤怒的锦鲤
 2 0 * * * node /scripts/jd_angryKoi.js >> /scripts/logs/jd_angryKoi.log 2>&1
+#全民抢京豆
+2 0 * * * node /scripts/jd_qjd.js >> /scripts/logs/jd_qjd.log 2>&1
