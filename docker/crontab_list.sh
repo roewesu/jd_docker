0e6f356190d3a47606de214addaf6eeaa8c2caa5
zhaozhanzhan <799316652@qq.com>
2021-08-14 10:13:43 +0800

    5G超级盲盒

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -16,7 +16,7 @@
 #金榜创造营 活动时间：2021-05-21至2021-12-31
 0 1,22 * * * node /scripts/jd_gold_creator.js >> /scripts/logs/jd_gold_creator.log 2>&1
 #5G超级盲盒(活动时间：2021-06-2到2021-07-31)
-0 0-23/4 * * * node /scripts/jd_mohe.js >> /scripts/logs/jd_mohe.log 2>&1
+0 0,1-23/3 * * * node /scripts/jd_mohe.js >> /scripts/logs/jd_mohe.log 2>&1
 #明星小店(星店长，2021-06-10)
 0 1,21 * * * node /scripts/jd_star_shop.js >> /scripts/logs/jd_star_shop.log 2>&1
 #省钱大赢家之翻翻乐
